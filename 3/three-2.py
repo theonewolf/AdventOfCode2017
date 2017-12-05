451041f5437ad9e70c596e02c4ee3f5f5fe21c74
Wolfgang Richter <wolfgang.richter@gmail.com>
2017-12-04 22:45:23 -0500

    3/three-2.py: bit of a cop out, found the sequence in OIES

---
+++ 3/three-2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# vim:set nospell:
+"""
+Implements the third challenge.
+
+Author: Wolfgang Richter <wolfgang.richter@gmail.com>
+
+Copyright 2017 Wolfgang Richter, under the MIT License.
+"""
+
+
+if __name__ == '__main__':
+    print('https://oeis.org/A141481/b141481.txt')
+    print('''59 279138
+60 295229''')
